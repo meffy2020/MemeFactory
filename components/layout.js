848e00985e915f64b05534800f4790bab08af603
meffy2020 <madksj@naver.com>
2024-06-05 02:01:55 +0900

    first

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import Header from './header';
+import Footer from './footer';
+
+
+
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <Header />
+           
+            <div>{children}</div>
+            <Footer />
+        </>
+    )
+}
